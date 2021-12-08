e9223be797287bc33d88f6a73d4bd45501eae276
Elio Marcolino <eliom@jfrog.com>
2021-12-08 14:59:24 -0800

    PIPE-7133 - Automated test cases

--- tests/scripts/utility.sh
+++ tests/scripts/utility.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
+# Execute a utility function to make sure they work
 uuid=$(get_uuid)
 echo "uuid: $uuid"
 
-[[ $(type -t get_uuid) == function ]] && echo "function found" || echo "function not found"
+# Check all utility functions are available
+fn_exists() { declare -F "$1" > /dev/null; }
+
+func_name="get_uuid"
+fn_exists $func_name && echo "$func_name found" || echo "$func_name not found"
+
+func_name="get_uuid2"
+fn_exists $func_name && echo "$func_name found" || echo "$func_name not found"
