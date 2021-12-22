da833b175e4002863057565970dcfc839a9a30fe
Elio Marcolino <eliom@jfrog.com>
2021-12-22 13:42:55 -0800

    PIPE-7133 - Test case

--- tests/scripts/utility.sh
+++ tests/scripts/utility.sh
@@ -6,9 +6,7 @@ echo "uuid: $uuid"
 
 # Check all utility functions are available
 fn_exists() { declare -F "$1" > /dev/null; }
-
-source utility_function_names.sh
-
+IFS='|' read -r -a utility_functions <<< "$utility_function_names"
 for func_name in "${utility_functions[@]}"
 do
   fn_exists $func_name && echo "$func_name found" || (echo "$func_name not found" && exit 99)
